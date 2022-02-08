dbbf79a3c4952bc2a38f59842088c340825abf0d
Tobias Prisching <tobias.prisching@icloud.com>
2022-02-08 10:35:51 +0100

    Update main.py
    Changed default authentication method to manual input

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from authentication import getAPIobject
 from download import downloadAll
 
 if __name__ == "__main__":
-    apiObject = getAPIobject(False)
+    apiObject = getAPIobject(True)
     downloadAll(apiObject)
